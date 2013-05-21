2cfd48d6ea2bc1350b2377b7590f41591e1ccd15
Paul Ser <pashka4281@gmail.com>
2013-05-22 01:19:30 +0300

    assets compile v1

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,8 @@ module TShirtUA
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    config.assets.enabled = true
+    config.assets.precompile += %w(store/shared/_print.css)
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
